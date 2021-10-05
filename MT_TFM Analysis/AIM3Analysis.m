5641257d9c9c71b467fb7aadcd5b74d04a48f534
Waddah Moghram <waddah-moghram@uiowa.edu>
2021-10-05 11:34:52 -0500

    MLU 396 changes

--- MT_TFM Analysis/AIM3Analysis.m
+++ MT_TFM Analysis/AIM3Analysis.m
@@ -574,6 +574,10 @@ format longg
     fprintf('DIC Tracking output is saved as: \n\t%s\n', MagBeadTrackedDisplacementsFullFileName);
 
     % ---------------------- Plotting  ----------------------
+        % Finding out what the last frame count is 
+    LastFrame_DIC = min([numel(TimeStampsRT_Abs_DIC), numel(BeadPositionXYdisplMicron), VeryLastFrame]);       
+    FramesDoneNumbersDIC = FirstFrame_DIC:LastFrame_DIC;
+    
     showPlots = 'on';
     figHandle = figure('visible',showPlots, 'color', 'w');     % added by WIM on 2019-02-07. To show, remove 'visible 
     plot(TimeStampsRT_Abs_DIC(FramesDoneNumbersDIC), BeadPositionXYdisplMicron(FramesDoneNumbersDIC,3), 'b-', 'LineWidth', 1)
