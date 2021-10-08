cce7de7507674501a90fc14b5d14f01b66827c0b
Waddah I. Moghram <waddah.moghram@gmail.com>
2021-10-08 09:47:43 -0500

    update to startup.m

--- Dependencies/startup.m
+++ Dependencies/startup.m
@@ -2,8 +2,8 @@
 threadCount = str2double(getenv('NUMBER_OF_PROCESSORS'))
 %gpuDev = gpuDevice
 delete(gcp('nocreate'))
-%localcluster = parcluster('local')
-%localcluster.NumThreads = 1;
-%localcluster.NumWorkers = threadCount;
+localcluster = parcluster('local')
+localcluster.NumThreads = 1;
+localcluster.NumWorkers = threadCount;
 %localcluster.parpool(threadCount)
 parpool(threadCount)
