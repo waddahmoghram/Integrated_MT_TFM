88c919a61ed87ffefa994a8de130e84dee70c5e8
Waddah Moghram <waddah-moghram@uiowa.edu>
2021-10-14 13:48:29 -0500

    resolve conflict with other branches

--- Dependencies/GelSample.m
+++ Dependencies/GelSample.m
@@ -1,11 +1,13 @@
 classdef GelSample
-    %UNTITLED2 Summary of this class goes here
+    %GelSample(SampleID)
     %   Detailed explanation goes here
 
     properties
         GelID_
         GelType_
         GelThickness_
+=======
+        GelThickness_micron
         GelDiameter_mm_
         pH_
         concentration_
@@ -14,17 +16,25 @@ classdef GelSample
         RedMicrospheres                         % Class
     end
 
+=======
+    properties (Constant)
+        GelDefaultDiameter_mm = 18;                     % 18 mm-diameter gels is the default
+        GelDefaultDiameGelTypeter_mm = 'Type I rat-tail collagen gel';
+        GelDefault_pH = 7.4;
+    end
+
     methods
         function obj = GelSample(varargin)
             %UNTITLED2 Construct an instance of this class
             %   Detailed explanation goes here
-            GelDefaultDiameter_mm = 18;                     % 18 mm-diameter gels is the default
-            GelDefaultDiameGelTypeter_mm = 'Type I rat-tail collagen gel';
-            GelDefault_pH = 7.4;
-
             ip = inputParser();
             ip.addRequired('GelID', @ischar);
             ip.addRequired('GelThickness_', @(x) validateattributes(x, {'numeric'}, {'scalar'}));
+
+
+            ip = inputParser();
+            ip.addRequired('GelID', @ischar);
+            ip.addRequired('GelThickness_micron', @(x) validateattributes(x, {'numeric'}, {'scalar'}));
             ip.addOptional('GelType', GelDefaultDiameGelTypeter_mm , @ischar);  
             ip.addOptional('GelDiameter_mm_', GelDefaultDiameter_mm, @(x) validateattributes(x, {'numeric'}, {'scalar'}));     % 18-mm default
             ip.addOptional('pH_', GelDefaultDiameter_mm, @(x) validateattributes(x, {'numeric'}, {'scalar'}));     % 18-mm default
